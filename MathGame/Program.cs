7d6c5550ba5623b85b859ba6ade342d7446ecfa8
bunchycrunch <sparksj1127@gmail.com>
2023-10-05 16:08:32 -0600

    added logic to each game

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,32 +1,142 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please type your name");
 
-var name = Console.ReadLine();
+
 var date = DateTime.UtcNow;
 
-Menu(name, date);
+var name = GetName();
+
+Menu(name);
+
+string GetName()
+{
+    Console.WriteLine("Please type your name");
+    var name = Console.ReadLine();
+    return name;
+}
 
 void AdditionGame(string message)
 {
     Console.WriteLine("Addition game selected");
+
+    var random = new Random();
+    var score = 0;
+    int firstNumber;
+    int secondNumber;
+
+    for(int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        if(int.Parse(result) == firstNumber + secondNumber)
+        {
+            Console.WriteLine("Your answer was correct!");
+            score++;
+        } 
+        else 
+        {
+            Console.WriteLine("Your answer was incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}.");
+    }
+
+
 }
 void SubtractionGame(string message)
 {
     Console.WriteLine("Subtraction game selected");
+
+    var random = new Random();
+    var score = 0;
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Your answer was correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}.");
+    }
 }
 void MultiplicationGame(string message)
 {
     Console.WriteLine("Multiplication game selected");
+
+    var random = new Random();
+    var score = 0;
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} * {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Your answer was correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}.");
+    }
 }
 void DivisionGame(string message)
 {
     Console.WriteLine("Division game selected");
+
+    var random = new Random();
+    var score = 0;
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} / {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber / secondNumber)
+        {
+            Console.WriteLine("Your answer was correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect.");
+        }
+
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}.");
+    }
 }
 
-void Menu(string? name, DateTime date)
+void Menu(string name)
 {
     Console.WriteLine("---------------------");
-    Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself\n");
+    Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math game\n");
     Console.WriteLine($@"What game would yo like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
